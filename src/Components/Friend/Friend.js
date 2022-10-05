59142b47179b50f91e26f4f97135489cbf5d72ee
faria-r <fariacse11@gmail.com>
2022-10-05 08:20:36 +0600

    Dynamic route created for single User

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Friend.css'
 
 const Friend = ({friend}) => {
-    const  {name,email,phone,username,website} = friend;
+    const  {name,id,email,phone,username,website} = friend;
     return (
         <div className='friend'>
             <p>Name:{name}</p>
             <h3>Email:{email}</h3>
             <p><small>Phone:{phone}</small></p>
-            <h2>UserName:{username}</h2>
+            <h2>UserName:<Link to={`/friend/${id}`}>{username}</Link></h2>
             <p>Website:{website}</p>
         </div>
     );
